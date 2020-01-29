3a56625db3765a1197372f12ebab1bc285f7ab01
Anik Ghosh <anikghosh@Aniks-MacBook-Air.local>
2020-01-29 22:31:21 +0530

    W2D2 CC3

--- coding-challenges/week02/Day3/cc2-w2d3.py
+++ coding-challenges/week02/Day3/cc2-w2d3.py
@@ -1,33 +1,33 @@
 def print_rangoli(size):
     value_a = ord('a')
 
-    for i in range(0, n):
+    for i in range(0, size):
 
-        print("-" * ((2 * (n - i)) - 2), end="")
+        print("-" * ((2 * (size - i)) - 2), end="")
         for j in range(0, i + 1):
             if j > 0:
                 print("-", end="")
-            print(chr(value_a + (n - j - 1)), end="")
+            print(chr(value_a + (size - j - 1)), end="")
 
         for k in range(i, 0, -1):
             print("-", end="")
-            print(chr(value_a + (n - k)), end="")
+            print(chr(value_a + (size - k)), end="")
 
-        print("-" * ((2 * (n - i)) - 2))
+        print("-" * ((2 * (size - i)) - 2))
 
-    for i in range(n - 2, -1, -1):
+    for i in range(size - 2, -1, -1):
 
-        print("-" * ((2 * (n - i)) - 2), end="")
+        print("-" * ((2 * (size - i)) - 2), end="")
         for j in range(0, i + 1):
             if j > 0:
                 print("-", end="")
-            print(chr(value_a + (n - j - 1)), end="")
+            print(chr(value_a + (size - j - 1)), end="")
 
         for k in range(i, 0, -1):
             print("-", end="")
-            print(chr(value_a + (n - k)), end="")
+            print(chr(value_a + (size - k)), end="")
 
-        print("-" * ((2 * (n - i)) - 2))
+        print("-" * ((2 * (size - i)) - 2))
 
 
 if __name__ == '__main__':
