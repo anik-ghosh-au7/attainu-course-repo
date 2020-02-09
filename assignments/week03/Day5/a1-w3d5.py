99866844b3e2a78bf8d6b00c0d7ebeba1a4703ba
Anik ghosh <tech.anikghosh@gmail.com>
2020-02-09 13:06:02 +0530

    Week 3 Day 5 Updated CCs and Assignments

--- assignments/week03/Day5/a1-w3d5.py
+++ assignments/week03/Day5/a1-w3d5.py
@@ -11,19 +11,48 @@
 #     [-1, -1]
 
 
-def position(lis, num):
-    for i in range(len(lis)):
-        if lis[i] == num:
-            for j in range(len(lis) - 1, -1, -1):
-                if lis[j] == num:
-                    print([i, j])
-                    break
-            break
-    else:
-        print([-1, -1])
+# def position(lis, num):
+#     for i in range(len(lis)):
+#         if lis[i] == num:
+#             for j in range(len(lis) - 1, -1, -1):
+#                 if lis[j] == num:
+#                     print([i, j])
+#                     break
+#             break
+#     else:
+#         print([-1, -1])
+
+# 2 4 10 10 10 18 20
+# 0 1 2  3  4  5  6
+# 10
+# [2,4]
+
+# 2 4 10 10 10 18 20
+# 9
+# [-1, -1]
+
+# runtime complexity O(log(n))
+
+def binary_search(arr, num, search_first):
+    low = 0
+    high = len(arr) - 1
+    result = -1
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] == num:
+            result = mid
+            if search_first:
+                high = mid - 1
+            else:
+                low = mid + 1
+        elif arr[mid] > num:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return result
 
 
 input_list = list(map(int, input().split()))
 n = int(input())
 
-position(input_list, n)
+print([binary_search(input_list, n, True), binary_search(input_list, n, False)])
