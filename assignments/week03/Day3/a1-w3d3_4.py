2f14525d00810b97cbb93631005bee735e2d5489
Anik ghosh <tech.anikghosh@gmail.com>
2020-02-14 03:01:21 +0530

    Week 3 Day 3_4 updated with inputs

--- assignments/week03/Day3/a1-w3d3_4.py
+++ assignments/week03/Day3/a1-w3d3_4.py
@@ -1,3 +1,13 @@
+# https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+# input
+# 5
+# 2 3 6 6 5
+
+# output
+# 5
+
+
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
