8d569580000bb8b6655267a7490776e473ee5b71
Anik ghosh <tech.anikghosh@gmail.com>
2020-06-03 22:16:15 +0530

    week 19 day 3 assignment

--- assignments/week19/Day 3/w19-day3-a/index.js
+++ assignments/week19/Day 3/w19-day3-a/index.js
@@ -4,13 +4,7 @@ import router from './routes/user.routes';
 
 const app = express();
 
-
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
-
-app.get('/list', router);
-app.post('/list/:item', router);
+app.use('/', router);
 
 app.use((req, res) => {
   res.status(404).send({msg: 'not a valid route'});
