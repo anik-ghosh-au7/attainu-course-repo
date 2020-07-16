7a70ab6b303c332f1046870b4b54b404778a75e5
Anik ghosh <tech.anikghosh@gmail.com>
2020-07-16 16:26:36 +0530

    week 25 day 4 assignment

---
+++ assignments/week25/Day4/w25day4a/index.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    createContent();
+};
+
+function createContent() {
+    document.body.innerHTML = `<div class='image'><img src='https://goo.gl/kjzfbE' alt='First'><button class='remove'>X</button></div></div>`
+};
