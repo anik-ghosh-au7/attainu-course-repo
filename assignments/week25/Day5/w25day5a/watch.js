c62ea19cd261737ebbf708e3e7495dfafd644f00
Anik ghosh <tech.anikghosh@gmail.com>
2020-07-17 20:26:05 +0530

    week 25 day 5 assignment

--- assignments/week25/Day5/w25day5a/watch.js
+++ assignments/week25/Day5/w25day5a/watch.js
@@ -1,27 +1,33 @@
 window.onload = () => {
     let min = document.getElementById('min');
     let sec = document.getElementById('sec');
-    let ms = document.getElementById('centi_sec');
+    let cs = document.getElementById('centi_sec');
 
     let start = document.getElementById('start');
     let stop = document.getElementById('stop');
     let reset = document.getElementById('reset');
 
-    let start_min, start_sec, start_ms;
+    let clock = document.getElementById('clock');
+    let out_clock = document.getElementById('out_clock');
+
+    let start_min, start_sec, start_cs;
 
     start.addEventListener('click', () => {
-        start_ms = setInterval(() => {
-            ms.innerText = parseInt(ms.innerText) + 1 > 9 ? parseInt(ms.innerText) + 1 : '0' + (parseInt(ms.innerText) + 1);
-            if (ms.innerText === '99') {
-                ms.innerText = '00';
+        start_cs = setInterval(() => {
+            cs.innerText = parseInt(cs.innerText) + 1 > 9 ? parseInt(cs.innerText) + 1 : '0' + (parseInt(cs.innerText) + 1);
+            if (cs.innerText === '99') {
+                cs.innerText = '00';
             }
+            clock.style.background = `conic-gradient(#cce467 ${parseInt(cs.innerText)}%, #ffc750 0 ${100 - (parseInt(cs.innerText))}%)`;
+            clock.innerText = parseInt(cs.innerText) > 9 ? parseInt(cs.innerText) : '0' + parseInt(cs.innerText);
         }, 10);
         start_sec = setInterval(() => {
             if (sec.innerText === '60') {
                 sec.innerText = '00';
             }
             sec.innerText = parseInt(sec.innerText) + 1 > 9 ? parseInt(sec.innerText) + 1 : '0' + (parseInt(sec.innerText) + 1);
-        },1000);
+            out_clock.style.background = `conic-gradient(#004ffa ${parseInt(sec.innerText)*1.67}%, #7bd3db 0 ${100 - (parseInt(sec.innerText)*1.66)}%)`;
+        },1000)
         start_min = setInterval(() => {
             if (min.innerText === '60') {
                 min.innerText = '00';
@@ -31,17 +37,20 @@ window.onload = () => {
     });
 
     stop.addEventListener('click', () => {
-        clearInterval(start_ms);
+        clearInterval(start_cs);
         clearInterval(start_sec);
         clearInterval(start_min);
     });
 
     reset.addEventListener('click', () => {
-        clearInterval(start_ms);
-        ms.innerText = '00';
+        clearInterval(start_cs);
+        cs.innerText = '00';
         clearInterval(start_sec);
         sec.innerText = '00';
         clearInterval(start_min);
         min.innerText = '00';
+        out_clock.style.background = `conic-gradient(#004ffa 0%, #7bd3db 0 100%)`;
+        clock.style.background = `conic-gradient(#cce467 100%, #ffc750 0%)`;
+        clock.innerText = '00';
     });
 };
