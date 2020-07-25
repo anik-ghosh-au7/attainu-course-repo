b7cb5f25d143fa0ba1e14e722ec733998c614618
Anik ghosh <tech.anikghosh@gmail.com>
2020-07-25 18:46:22 +0530

    FE test JULY

---
+++ Monthly_test/July/FE/question1/index.js
@@ -0,0 +1,9 @@
+function zoom_in() {
+    document.getElementById('profile-img').style.width = '200px';
+    document.getElementById('profile-img').style.height = '220px';
+}
+
+function zoom_out() {
+    document.getElementById('profile-img').style.width = '180px';
+    document.getElementById('profile-img').style.height = '200px';
+}
