fb760f10bf16a7f4d8aaa676846a36447ae0177b
Anik ghosh <tech.anikghosh@gmail.com>
2020-08-05 22:52:08 +0530

    week 28 day 3 assignment

---
+++ assignments/week28/Day3/w28day3a/build/precache-manifest.7d23bfdab6e1a715843133df8488f77a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ea597571d53031063f3e22c2dce84d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3186f20955a1914db341",
+    "url": "/static/css/main.cfc4900b.chunk.css"
+  },
+  {
+    "revision": "f89c867f50a6da58573d",
+    "url": "/static/js/2.ab4d023c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ab4d023c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3186f20955a1914db341",
+    "url": "/static/js/main.7f539f07.chunk.js"
+  },
+  {
+    "revision": "6e081593a8b1feebddcd",
+    "url": "/static/js/runtime-main.bb37f1aa.js"
+  }
+]);
