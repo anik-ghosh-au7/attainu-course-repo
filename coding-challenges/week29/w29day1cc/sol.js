417cd495a5539414d11c86ca90c7b3b99cf212ff
Anik ghosh <tech.anikghosh@gmail.com>
2020-08-10 15:48:46 +0530

    week 29 day 1 cc

--- coding-challenges/week29/w29day1cc/sol.js
+++ coding-challenges/week29/w29day1cc/sol.js
@@ -1,15 +1,29 @@
+// Method 1
+
+// const removeDuplicates = (arr) => {
+//     let count = 0;
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i-1] === arr[i]) {
+//             count++
+//         }
+//         if (count > 1) {
+//             arr.splice(i, 1);
+//             i--;
+//         }
+//         if (arr[i] !== arr[i+1]) {
+//             count = 0;
+//         }
+//     }
+//     return arr.length;
+// };
+
+// Method 2
 const removeDuplicates = (arr) => {
-    let count = 0;
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i-1] === arr[i]) {
-            count++
-        }
-        if (count > 1) {
+    if (arr.length < 2) return arr.length;
+    for (let i = 2; i < arr.length; i++) {
+        if (arr[i-1] === arr[i] & arr[i-2] === arr[i]) {
             arr.splice(i, 1);
             i--;
-        }
-        if (arr[i] !== arr[i+1]) {
-            count = 0;
         }
     }
     return arr.length;
