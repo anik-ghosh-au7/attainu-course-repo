cc409cc1e64c1189eb3e581a41be92cabeafbabc
Anik ghosh <tech.anikghosh@gmail.com>
2020-08-21 14:01:51 +0530

    week 30 day 5 cc optimized

--- coding-challenges/week30/Day5/w30day5cc/sol.js
+++ coding-challenges/week30/Day5/w30day5cc/sol.js
@@ -1,7 +1,8 @@
 const valid_binary_tree = (tree) => {
     let i = 0;
-    while (i < tree.length/2) {
+    while (i < tree.length/2+2) {
         if(tree[i]) {
+            // console.log(`${tree[2*i+1]}<${tree[i]} && ${tree[2*i+2]}>${tree[i]}`);
             if(tree[2*i+1] && tree[2*i+1]>tree[i]) return false;
             if(tree[2*i+2] && tree[2*i+2]<tree[i]) return false;
         }
@@ -10,6 +11,7 @@ const valid_binary_tree = (tree) => {
     return true;
 };
 
-console.log(valid_binary_tree([2,1,3])); // true
-console.log(valid_binary_tree([5,1,4,null,null,3,6])); // false
-console.log(valid_binary_tree([8,3,10,1,6,null,14,null,null,4,7,13])); // true
+// console.log(valid_binary_tree([2,1,3])); // true
+// console.log(valid_binary_tree([5,1,4,null,null,3,6])); // false
+console.log(valid_binary_tree([8,3,10,1,6,null,14,null,null,4,7,null,null,13])); // true
+console.log(valid_binary_tree([8,3,10,1,6,null,14,null,null,4,7,null,null,15])); // false
