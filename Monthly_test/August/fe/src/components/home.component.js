367bd75426fa0948504f824ac00527c4c2c64753
Anik ghosh <tech.anikghosh@gmail.com>
2020-08-29 20:20:04 +0530

    sign out added

--- Monthly_test/August/fe/src/components/home.component.js
+++ Monthly_test/August/fe/src/components/home.component.js
@@ -45,105 +45,128 @@ class Post extends React.Component {
   addLikeHandler = (id) => {
     this.props.addLike(id);
   };
+
+  clickedLogoutButton = async (e) => {
+    e.preventDefault();
+    console.log("logout button clicked");
+    const { history } = this.props;
+    localStorage.clear();
+    history.push("/sign-in");
+  };
   render() {
     const { posts } = this.props;
     const { title, comment_body } = this.state;
     return (
-      <div className="auth-inner">
-        <div className="container">
-          <div className="row">
-            <div className="span4 well">
-              <div className="row">
-                <div className="aboutus">
-                  <h2 className="aboutus-title">Posts</h2>
-                  <div>
-                    <form onSubmit={this.submitHandler}>
-                      <input
-                        type="text"
-                        name="title"
-                        value={title}
-                        onChange={this.inputHandler}
-                        style={{ width: "300px", marginBottom: "20px" }}
-                        placeholder="Add a post"
-                      />
-                      <button
-                        type="submit"
-                        className="btn btn-primary"
-                        title="add"
-                        disabled={!title}
-                      >
-                        Add
-                      </button>
-                    </form>
-                  </div>
-                  <div>
-                    <ul style={{ listStyleType: "none", paddingLeft: "0px" }}>
-                      {posts.map((post, idx) => {
-                        return (
-                          <Fragment key={idx}>
-                            {post.title}
-                            <button
-                              className="btn btn-danger"
-                              title="delete"
-                              onClick={() => this.deletePostHandler(post.id)}
-                            >
-                              Delete
-                            </button>
-                            {post.liked ? (
+      <>
+        <div className="auth-inner">
+          <div className="container">
+            <div className="row">
+              <div className="span4 well">
+                <div className="row">
+                  <div className="aboutus">
+                    <h2 className="aboutus-title">Posts</h2>
+                    <div>
+                      <form onSubmit={this.submitHandler}>
+                        <input
+                          type="text"
+                          name="title"
+                          value={title}
+                          onChange={this.inputHandler}
+                          style={{ width: "300px", marginBottom: "20px" }}
+                          placeholder="Add a post"
+                        />
+                        <button
+                          type="submit"
+                          className="btn btn-primary"
+                          title="add"
+                          disabled={!title}
+                        >
+                          Add
+                        </button>
+                      </form>
+                    </div>
+                    <div>
+                      <ul style={{ listStyleType: "none", paddingLeft: "0px" }}>
+                        {posts.map((post, idx) => {
+                          return (
+                            <Fragment key={idx}>
+                              {post.title}
                               <button
-                                className="btn btn-warning"
-                                title="like"
-                                onClick={() => this.addLikeHandler(post.id)}
+                                className="btn btn-danger"
+                                title="delete"
+                                onClick={() => this.deletePostHandler(post.id)}
                               >
-                                Unlike
+                                Delete
                               </button>
-                            ) : (
-                              <button
-                                className="btn btn-info"
-                                title="like"
-                                onClick={() => this.addLikeHandler(post.id)}
+                              {post.liked ? (
+                                <button
+                                  className="btn btn-warning"
+                                  title="like"
+                                  onClick={() => this.addLikeHandler(post.id)}
+                                >
+                                  Unlike
+                                </button>
+                              ) : (
+                                <button
+                                  className="btn btn-info"
+                                  title="like"
+                                  onClick={() => this.addLikeHandler(post.id)}
+                                >
+                                  Like
+                                </button>
+                              )}
+                              <form
+                                onSubmit={this.commentSubmitHandler(post.id)}
                               >
-                                Like
-                              </button>
-                            )}
-                            <form onSubmit={this.commentSubmitHandler(post.id)}>
-                              <input
-                                type="text"
-                                name="comment_body"
-                                value={comment_body}
-                                onChange={this.inputCommentHandler}
-                                style={{ width: "150px", marginBottom: "20px" }}
-                                placeholder="Add a comment"
-                              />
-                              <button
-                                type="submit"
-                                className="btn btn-primary"
-                                title="add"
-                                disabled={!comment_body}
-                              >
-                                Comment
-                              </button>
-                            </form>
-                            <ul>
-                              {post.comments.map((comment, idx) => {
-                                return (
-                                  <li key={`${idx}-comment`}>
-                                    Comment : {comment}
-                                  </li>
-                                );
-                              })}
-                            </ul>
-                          </Fragment>
-                        );
-                      })}
-                    </ul>
+                                <input
+                                  type="text"
+                                  name="comment_body"
+                                  value={comment_body}
+                                  onChange={this.inputCommentHandler}
+                                  style={{
+                                    width: "150px",
+                                    marginBottom: "20px",
+                                  }}
+                                  placeholder="Add a comment"
+                                />
+                                <button
+                                  type="submit"
+                                  className="btn btn-primary"
+                                  title="add"
+                                  disabled={!comment_body}
+                                >
+                                  Comment
+                                </button>
+                              </form>
+                              <ul>
+                                {post.comments.map((comment, idx) => {
+                                  return (
+                                    <li key={`${idx}-comment`}>
+                                      Comment : {comment}
+                                    </li>
+                                  );
+                                })}
+                              </ul>
+                            </Fragment>
+                          );
+                        })}
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <button
+          id="logout"
+          type="button"
+          className="btn btn-danger btn-block btn-sm"
+          onClick={this.clickedLogoutButton}
+        >
+          Sign Out
+        </button>
+      </>
     );
   }
 }
