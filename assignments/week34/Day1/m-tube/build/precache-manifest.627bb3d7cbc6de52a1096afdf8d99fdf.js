63ab06c79db5eb62cd91ac8204340e8df05dba8d
Anik ghosh <tech.anikghosh@gmail.com>
2020-09-14 20:19:33 +0530

    week 34 day 1 assignment

---
+++ assignments/week34/Day1/m-tube/build/precache-manifest.627bb3d7cbc6de52a1096afdf8d99fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1170f3c2b22f91cc285d9c6470a625d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c86bbb6075b1ba77d07",
+    "url": "/static/css/main.abd58810.chunk.css"
+  },
+  {
+    "revision": "cd4641ab908f1746ef41",
+    "url": "/static/js/2.3e591748.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e591748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c86bbb6075b1ba77d07",
+    "url": "/static/js/main.d6692f18.chunk.js"
+  },
+  {
+    "revision": "6a040985a61f21896475",
+    "url": "/static/js/runtime-main.8ccca7a7.js"
+  },
+  {
+    "revision": "37194ebb0d43ab3274bf6760a7e95262",
+    "url": "/static/media/Loading-Image-1.37194ebb.gif"
+  }
+]);
