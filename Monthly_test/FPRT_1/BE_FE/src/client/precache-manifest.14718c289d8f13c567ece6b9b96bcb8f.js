da7a4138aaf42ee6cf12cd9b3423f531184aec12
Anik ghosh <tech.anikghosh@gmail.com>
2020-10-21 12:09:10 +0530

    project structure and required packages for both BE and FE

---
+++ Monthly_test/FPRT_1/BE_FE/src/client/precache-manifest.14718c289d8f13c567ece6b9b96bcb8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09321edd914b9fd04a85d623508383b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82926c1f972a88259c32",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "52f0d3f54f1e0c56d0e3",
+    "url": "/static/js/2.958862ad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.958862ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82926c1f972a88259c32",
+    "url": "/static/js/main.5bebf3f0.chunk.js"
+  },
+  {
+    "revision": "c0a8995ca2cd209ab074",
+    "url": "/static/js/runtime-main.032126c5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
